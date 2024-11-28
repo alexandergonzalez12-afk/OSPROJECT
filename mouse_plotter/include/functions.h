a27ea77085d90390462f091fde4653887da7cfe9
alexandergonzalez12-afk <alexander.gonzalez12@upr.edu>
2024-11-27 21:10:05 -0400

    functions .h commented

--- mouse_plotter/include/functions.h
+++ mouse_plotter/include/functions.h
@@ -6,14 +6,44 @@
 #define TRAIL_LENGTH 10  // Length of the trail effect
 
 // Declare variables as extern
+
+/**
+ * @brief File pointer to the input binary file containing mouse data.
+ */
 extern FILE *input_file;
+
+/**
+ * @brief Path to the binary file containing mouse data.
+ */
 extern const char *file_path;
+
+/**
+ * @brief Array to store the X positions of the trail.
+ */
 extern int trail_x[TRAIL_LENGTH];
+
+/**
+ * @brief Array to store the Y positions of the trail.
+ */
 extern int trail_y[TRAIL_LENGTH];
+
+/**
+ * @brief Current index in the trail arrays.
+ */
 extern int trail_index;
+
+/**
+ * @brief Coordinates read from the file.
+ */
 extern int x, y;
 
 // Function declarations
+
+/**
+ * @brief Signal handler function to read mouse data and update the display.
+ * 
+ * @param signum The signal number that triggered the handler.
+ */
 void DrawTrace(int signum);
 
 #endif  // FUNCTIONS_H
