58262e7e893e10b7920fb2d2c52d0f68d7fcad70
alexandergonzalez12-afk <alexander.gonzalez12@upr.edu>
2024-11-27 21:07:22 -0400

    commented functions.h

--- mouse_saver/include/functions.h
+++ mouse_saver/include/functions.h
@@ -5,18 +5,44 @@
 
 #define TERM_WIDTH 100
 #define TERM_HEIGHT 25
+
 // Global variables for tracking file and mouse movements
 
+/**
+ * @brief File to save the transformed coordinates.
+ */
 extern FILE *output_transformed_file;
-extern FILE *output_file;
-extern float min_x, max_x; // Minimum and maximum X values
-extern float min_y, max_y; // Minimum and maximum Y values
-extern float x, y;         // Start mouse in the middle of terminal
 
+/**
+ * @brief File to save the mouse coordinates.
+ */
+extern FILE *output_file;
+
+/**
+ * @brief Minimum and maximum X values.
+ */
+extern float min_x, max_x;
+
+/**
+ * @brief Minimum and maximum Y values.
+ */
+extern float min_y, max_y;
+
+/**
+ * @brief Start mouse in the middle of terminal.
+ */
+extern float x, y;
+
+/**
+ * @brief Signal handler function.
+ * 
+ * @param sig The signal number.
+ */
 void SignalHandler(int sig);
 
 /**
  * @brief Ensures the binary file exists. If not, it creates it and opens it for appending.
+ * 
  * @param filePath The path to the binary file.
  * @return FILE* Pointer to the opened file.
  */
